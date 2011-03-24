72626d18052ca0a02fa8922726a01698453270ac
tumba25 <tumba25@gmail.com>
2011-03-24 23:30:38 +0200

    Initial commit. SFS support added and minor bugs fixed.

---
+++ root/adm/ocban.php
@@ -0,0 +1,205 @@
+<?php
+/*
+* START CONFIG
+*/
+
+// Ban the username?
+$ban_username = true;
+
+/* Set to the group ID if you want to move a user to a certain group.  This will set that group as default and remove the user from any other groups */
+$move_to_group = 9;
+
+// Delete the user's posts?
+$delete_posts = true;
+
+// Delete the user's avatar?
+$delete_avatar = true;
+
+// Delete the user's signature?
+$delete_signature = true;
+
+// Delete profile fields (website, IM addresses, location, etc)
+$delete_profile_fields = true;
+
+// If you want to report spammers automatically to StopForumSpam you need a API key.
+// http://www.stopforumspam.com/apikey
+$sfs_api_key = '';
+
+/*
+* END CONFIG
+*/
+
+define('IN_PHPBB', true);
+define('ADMIN_START', true);
+define('NEED_SID', true);
+
+// Include files
+$phpbb_root_path = (defined('PHPBB_ROOT_PATH')) ? PHPBB_ROOT_PATH : './../';
+$phpEx = substr(strrchr(__FILE__, '.'), 1);
+require($phpbb_root_path . 'common.' . $phpEx);
+require($phpbb_root_path . 'includes/functions_admin.' . $phpEx);
+require($phpbb_root_path . 'includes/functions_user.' . $phpEx);
+
+// Start session management
+$user->session_begin();
+$auth->acl($user->data);
+$user->setup(array('acp/common', 'mods/ocban'));
+// End session management
+
+// Have they authenticated (again) as an admin for this session?
+if (empty($user->data['session_admin']))
+{
+	login_box('', $user->lang['LOGIN_ADMIN_CONFIRM'], $user->lang['LOGIN_ADMIN_SUCCESS'], true, false);
+}
+
+// Is user any type of admin?
+if (!$auth->acl_get('a_'))
+{
+	trigger_error('NO_ADMIN');
+}
+
+$user_id = request_var('u', 0);
+
+if (!$user_id)
+{
+	trigger_error('NO_USER');
+}
+
+$sql = 'SELECT * FROM ' . USERS_TABLE . ' WHERE user_id = ' . $user_id;
+$result = $db->sql_query($sql);
+$row = $db->sql_fetchrow($result);
+$db->sql_freeresult($result);
+
+if (!$row)
+{
+	trigger_error('NO_USER');
+}
+
+$username = $row['username'];
+$user_ip = $row['user_ip'];
+$user_email = $row['user_email'];
+
+if ($row['user_type'] == USER_FOUNDER)
+{
+	trigger_error('You can not ban a founder');
+}
+// If the user selected cancel, redirect to the profile.
+if (isset($_POST['cancel']))
+{
+	redirect(append_sid($phpbb_root_path . 'memberlist.' . $phpEx, 'mode=viewprofile&amp;u=' . $user_id));
+	exit;
+}
+
+if (confirm_box(true))
+{
+	if ($ban_username)
+	{
+		user_ban('user', $username, 0, '', 0, '');
+	}
+
+	if ($delete_posts)
+	{
+		delete_posts('poster_id', $user_id);
+	}
+
+	if ($delete_avatar)
+	{
+		avatar_delete('user', $row, true);
+	}
+
+	if ($delete_signature)
+	{
+		$db->sql_query('UPDATE ' . USERS_TABLE . ' SET ' .
+			$db->sql_build_array('UPDATE', array('user_sig' => '', 'user_sig_bbcode_uid' => '', 'user_sig_bbcode_bitfield' => '')) . '
+			WHERE user_id = ' . $user_id);
+	}
+
+	if ($delete_profile_fields)
+	{
+		$sql_ary = array(
+			'user_birthday' 	=> '',
+			'user_from'			=> '',
+			'user_icq'			=> '',
+			'user_aim'			=> '',
+			'user_yim'			=> '',
+			'user_msnm'			=> '',
+			'user_jabber'		=> '',
+			'user_website'		=> '',
+			'user_occ'			=> '',
+			'user_interests'	=> '',
+			'user_phpbbcom'	=> '',
+			'user_gender'	=> 0,
+		);
+		$db->sql_query('UPDATE ' . USERS_TABLE . ' SET ' .
+			$db->sql_build_array('UPDATE', $sql_ary) . '
+			WHERE user_id = ' . $user_id);
+
+		// Also delete all extra profile fields for that user.
+		$db->sql_query('DELETE FROM ' . PROFILE_FIELDS_DATA_TABLE . '
+									 WHERE user_id = ' . $user_id);
+	}
+
+	if ($move_to_group)
+	{
+		$sql = 'SELECT group_id FROM ' . USER_GROUP_TABLE . ' WHERE user_id = ' . $user_id;
+		$result = $db->sql_query($sql);
+		while ($row = $db->sql_fetchrow($result))
+		{
+			group_user_del($row['group_id'], array($user_id), array($username));
+		}
+		$db->sql_freeresult($result);
+
+		group_user_add($move_to_group, array($user_id), array($username), false, true);
+	}
+
+	// Report spammer to SFS
+	if (!empty($sfs_api_key))
+	{
+		$http_request = 'http://www.stopforumspam.com/add.php';
+		$http_request .= '?username=' . $username;
+		$http_request .= '&ip_addr=' . $user_ip;
+		$http_request .= '&email=' . $user_email;
+		$http_request .= '&api_key=' . $sfs_api_key;
+
+		$response = file_get_contents($http_request);
+
+		if (strpos($response, 'Data submitted successfully') !== false)
+		{
+			// Successful
+			$message = $user->lang['SFS_SUCCESSFUL'];
+		}
+		else
+		{
+			$error_div = strpos($response, '<div class="msg error">') + 23;
+			$error_end = strpos($response, '</div>', $error_div);
+			$error_p = strpos($response, '<p>', $error_end) + 3;
+			$error_p_end = strpos($response, '</p>', $error_p);
+
+			$error = substr($response, $error_div, ($error_end - $error_div)) . '<br />';
+			$error .= substr($response, $error_p, ($error_p_end - $error_p)) . '<br />';
+
+			$message = $message = $user->lang['SFS_UNSUCCESSFUL'] . $error;
+		}
+	}
+	else
+	{
+		$message = '';
+	}
+
+	trigger_error(sprintf($user->lang['OCBAN_COMPLETE'], $message, append_sid("{$phpbb_root_path}memberlist.$phpEx", "mode=viewprofile&amp;u=$user_id")));
+}
+else
+{
+	confirm_box(false, sprintf($user->lang['OCBAN_CONFIRM'],
+			$username,
+			($ban_username) ? '' : $user->lang['NOT'],
+			($move_to_group) ? $move_to_group : $user->lang['N/A'],
+			($delete_posts) ? '' : $user->lang['NOT'],
+			($delete_avatar) ? '' : $user->lang['NOT'],
+			($delete_signature) ? '' : $user->lang['NOT'],
+			($delete_profile_fields) ? '' : $user->lang['NOT'],
+			(empty($sfs_api_key)) ? '' : $user->lang['AND_REPORT']
+	));
+}
+
+?>
---
+++ root/language/en/mods/ocban.php
@@ -0,0 +1,50 @@
+<?php
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+// DEVELOPERS PLEASE NOTE
+//
+// All language files should use UTF-8 as their encoding and the files must not contain a BOM.
+//
+// Placeholders can now contain order information, e.g. instead of
+// 'Page %s of %s' you can (and should) write 'Page %1$s of %2$s', this allows
+// translators to re-order the output of data while ensuring it remains correct
+//
+// You do not need this where single placeholders are used, e.g. 'Message %d' is fine
+// equally where a string contains only two placeholders which are used to wrap text
+// in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
+//
+// Some characters you may want to copy&paste:
+// ’ » “ ” …
+//
+
+$lang = array_merge($lang, array(
+	'AND_REPORT' => 'and report the user to SFS',
+
+	'N/A'				=> 'N/A',
+	'NOT'				=> '<strong>Not</strong>',
+
+	'OCBAN_COMPLETE'	=> 'User successfully banned%1$s.<br /><a href="%2$s">Click here to return to the user’s profile.</a>',
+	'OCBAN_CONFIRM'		=> 'Are you sure you want to one click ban %1$s?<br /><br />This will:<br />
+%2$s ban this user’s username<br />
+move the user to group %3$s<br />
+%4$s delete the user’s posts<br />
+%5$s delete the user’s avatar<br />
+%6$s delete the user’s signature<br />
+%7$s delete the user’s profile fields<br />
+%8$s',
+
+	'SFS_SUCCESSFUL' => ' and reported to StopForumSpam',
+	'SFS_UNSUCCESSFUL' => ' but there where some error reporting to StopForumSpam. The error message from SFS can be found below<br /><br />',
+
+	'USER_OCBAN'		=> 'OC Ban',
+));
+
+?>
